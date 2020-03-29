a4dbe33858b8636efbc6c8eeab0ca63eb43d572f
K. M <kristofer@munsterhjelm.no>
2020-03-29 14:58:15 +0200

    IMP: DisplayIOError: Replace numeric error codes with English.

--- src/fileops.pas
+++ src/fileops.pas
@@ -28,6 +28,7 @@
 unit Fileops;
 
 interface
+	function ErrorString(e: Integer): string;
 	procedure OpenForRead(var f: file; l: LongInt);
 	procedure OpenForRead(var f: TypedFile);
 	procedure OpenForRead(var f: Text);
@@ -42,6 +43,64 @@ interface
 
 implementation
 uses Dos;
+
+{https://forum.lazarus.freepascal.org/index.php/topic,46911.0.html}
+function ErrorString(e: Integer): string;
+	var
+		numericError: string;
+	begin
+		case e of
+			  1: ErrorString := 'Invalid function number';
+			  2: ErrorString := 'File not found.';
+			  3: ErrorString := 'Path not found.';
+			  4: ErrorString := 'Too many open files.';
+			  5: ErrorString := 'Access denied.';
+			  6: ErrorString := 'Invalid file handle.';
+			 12: ErrorString := 'Invalid file-access mode.';
+			 15: ErrorString := 'Invalid disk number.';
+			 16: ErrorString := 'Cannot remove current directory.';
+			 17: ErrorString := 'Cannot rename across volumes.';
+			100: ErrorString := 'Error when reading from disk.';
+			101: ErrorString := 'Error when writing to disk.';
+			102: ErrorString := 'File not assigned.';
+			103: ErrorString := 'File not open.';
+			104: ErrorString := 'File not opened for input.';
+			105: ErrorString := 'File not opened for output.';
+			106: ErrorString := 'Invalid number.';
+			150: ErrorString := 'Disk is write protected.';
+			151: ErrorString := 'Unknown device.';
+			152: ErrorString := 'Drive not ready.';
+			153: ErrorString := 'Unknown command.';
+			154: ErrorString := 'CRC check failed.';
+			155: ErrorString := 'Invalid drive specified..';
+			156: ErrorString := 'Seek error on disk.';
+			157: ErrorString := 'Invalid media type.';
+			158: ErrorString := 'Sector not found.';
+			159: ErrorString := 'Printer out of paper.';
+			160: ErrorString := 'Error when writing to device.';
+			161: ErrorString := 'Error when reading from device.';
+			162: ErrorString := 'Hardware failure.';
+			200: ErrorString := 'Division by zero';
+			201: ErrorString := 'Range check error';
+			202: ErrorString := 'Stack overflow error';
+			203: ErrorString := 'Heap overflow error';
+			204: ErrorString := 'Invalid pointer operation';
+			205: ErrorString := 'Floating point overflow';
+			206: ErrorString := 'Floating point underflow';
+			207: ErrorString := 'Invalid floating point operation';
+			210: ErrorString := 'Object not initialized';
+			211: ErrorString := 'Call to abstract method';
+			212: ErrorString := 'Stream registration error';
+			213: ErrorString := 'Collection index out of range';
+			214: ErrorString := 'Collection overflow error';
+			215: ErrorString := 'Arithmetic overflow error';
+			216: ErrorString := 'General protection fault';
+			else begin
+				Str(e, numericError);
+				ErrorString := '#' + numericError;
+			end;
+		end;
+	end;
 
 procedure OpenForRead(var f: file; l: LongInt);
 	begin
